b9727d1027a568a1ffc338cd78272f6ee6bebcbe
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-01-12 16:14:41 +0100

    added p2p communication chat

--- node.go
+++ node.go
@@ -2,49 +2,158 @@ package main
 
 import (
 	"github.com/przemekBielak/blockchain"
-	// "github.com/libp2p/go-libp2p-host"
+	libp2p "github.com/libp2p/go-libp2p"
+	crypto "github.com/libp2p/go-libp2p-crypto" 
+	ma "github.com/multiformats/go-multiaddr"
+	peerstore "github.com/libp2p/go-libp2p-peerstore"
+	"github.com/libp2p/go-libp2p-net"
+	"context"
 	"fmt"
 	"crypto/rand"
-	"crypto/rsa"
-	"golang.org/x/crypto/ed25519" 
+	"log"
+	"flag"
+	"bufio"
+	"os"
 )
 
 
 var myBlockchain blockchain.Blockchain
 
+func handleStream(s net.Stream) {
+	log.Println("Got a new stream!")
 
-func createHost(port int) {
-	fmt.Println(port)
+	// Create a buffer stream for non blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
 
-	// random number
-	r := rand.Reader
+	go readData(rw)
+	go writeData(rw)
 
-	// generate private/public keys
-	privateKey, err := rsa.GenerateKey(r, 2048)
-	if err != nil {
-		fmt.Println(err.Error)
+	// stream 's' will stay open until you close it (or the other side closes it).
+}
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, _ := rw.ReadString('\n')
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[32m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+
 	}
-	publicKey := &privateKey.PublicKey
+}
 
-	fmt.Println(privateKey)
-	fmt.Println(publicKey)
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
 
-	pub, priv, err := ed25519.GenerateKey(rand.Reader)
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+
+		if err != nil {
+			panic(err)
+		}
+
+		rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		rw.Flush()
+	}
+
+}
+
+func createHost() {
+	sourcePortPtr := flag.Int("sp", 0, "This peer source port")
+	destAddrPtr := flag.String("d", "", "Destination multiaddr string")
+	flag.Parse()
+
+	// priv, pub, err
+	priv, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("priv:", priv)
-	fmt.Println("pub:", pub)
+	SrcMultiAddr, err := ma.NewMultiaddr(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", *sourcePortPtr))
+	if err != nil {
+		panic(err)
+	}
 
-	data := []byte{123, 123, 123, 124}
-	sign := ed25519.Sign(priv, data)
+	// The context governs the lifetime of the libp2p node
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	fmt.Println("signature:", sign)
+	host, err := libp2p.New(
+		ctx,
+		libp2p.Identity(priv),
+		libp2p.ListenAddrs(SrcMultiAddr),
+	)
+	if err != nil {
+		panic(err)
+	}
 
-	fakedSign := []byte{123, 231, 143}
-	fmt.Println(ed25519.Verify(pub, data, fakedSign))
+	// If destination address is not provided, act as a host
+	if *destAddrPtr == "" {
+		host.SetStreamHandler("/chat/1.0.0", handleStream)
+
+		// Get the actual TCP port from listen multiaddr
+		// 0 - random available port
+		// otherwise *sourcePortPtr from cli argument
+		var port string
+		for _, addr := range host.Addrs() {
+			p, err := addr.ValueForProtocol(ma.P_TCP) 
+			if err == nil {
+				port = p
+				break
+			}
+		}
+
+		if port == "" {
+			panic("was not able to find actual local port")
+		}
+
+		fmt.Printf("Run './chat -d /ip4/127.0.0.1/tcp/%v/p2p/%s' on another console.\n", port, host.ID().Pretty())
+		fmt.Println("You can replace 127.0.0.1 with public IP as well.")
+		fmt.Printf("\nWaiting for incoming connection\n\n")
+
+		// Hang forever
+		<-make(chan struct{})
+	} else {
+		DstMultiAddr, err := ma.NewMultiaddr(*destAddrPtr)
+		if err != nil {
+			panic(err)
+		}
+
+		peerInfo, err := peerstore.InfoFromP2pAddr(DstMultiAddr)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		fmt.Println("This nodes multiaddress:")
+		for _, addr := range host.Addrs() {
+			fmt.Println(addr)
+		}
+		fmt.Println(host.ID().Pretty());
+		fmt.Println()
 	
+		fmt.Println("Peer multiaddress:")
+		fmt.Println(peerInfo.Addrs)
+		fmt.Println(peerInfo.ID.Pretty())
+
+		host.Peerstore().AddAddrs(peerInfo.ID, peerInfo.Addrs, peerstore.PermanentAddrTTL)
+
+		s, err := host.NewStream(context.Background(), peerInfo.ID, "/chat/1.0.0")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+		
+		go writeData(rw)
+		go readData(rw)
+
+		select {}
+	}	
 }
 
 func main() {
@@ -54,6 +163,7 @@ func main() {
 
 	fmt.Println(myBlockchain)
 
-	createHost(123)
+	createHost()
 }
+
 
